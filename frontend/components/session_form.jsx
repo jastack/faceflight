f343b7b8d197442212048cf3d7e9d9770453a4c8
James Stack <james.a.stack@gmail.com>
2017-05-31 17:41:59 -0700

    Fix styling

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
+import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+  console.log(response);
+};
 
 class SessionForm extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { username: "", password: "" };
+		this.state = { username: "", password: "", formType: "login" };
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.clearErrors = this.clearErrors.bind(this);
 		this.guestLogin = this.guestLogin.bind(this);
-	}
+    this.processForm = this.processForm.bind(this);
+    this.signup = this.signup.bind(this);
+  }
 
 
 
@@ -26,8 +33,17 @@ class SessionForm extends React.Component {
 	handleSubmit(e) {
 		e.preventDefault();
 		const user = this.state;
-		this.props.processForm(user).then(this.props.closeModal);
+		this.processForm(user);
 	}
+
+  processForm(user){
+    if (this.state.formType === "login") {
+      console.log("got here!");
+      this.props.login(user);
+    } else {
+      console.log("not quite ready");
+    }
+  }
 
 
 	renderErrors() {
@@ -47,9 +63,9 @@ class SessionForm extends React.Component {
 	}
 
 	renderSubmit(){
-		if (this.props.formType === 'login') {
+		if (this.state.formType === 'login') {
 			return (
-					<input type="submit" value="Sign in" />
+					<input type="submit" value="Log In" />
 			);
 		} else {
 			return (
@@ -64,14 +80,19 @@ class SessionForm extends React.Component {
 		this.props.login({username: "Harry", password: "password1"}).then(this.props.closeModal);
 	}
 
+  signup(e){
+    e.preventDefault();
+    this.setState({formType: "signup"});
+  }
+
 
 	render() {
+    console.log(this.props);
 		return (
-			<div className="login-form-container">
+      <div className="main">
+			  <div className="login-form-container">
 					<div className="box">
-						<header className="head">
 							<img src="http://res.cloudinary.com/dopv3qpj7/image/upload/v1496209211/plane_u4vmfh.svg"></img>
-						</header>
 
 					<form onSubmit={this.handleSubmit} className="login-form-box">
 						<ul>
@@ -93,8 +114,26 @@ class SessionForm extends React.Component {
 								{this.renderSubmit()}
 							</div>
 					</form>
+          <div className="line">
+            <hr /> <p>or</p> <hr />
+          </div>
+          <FacebookLogin
+            appId="1509388585748949"
+            autoLoad={false}
+            size="small"
+            fields="name,email,picture"
+            cssClass="my-facebook-button-class"
+            icon="fa-facebook-official"
+            textButton="Log in with Facebook"
+            callback={responseFacebook} />
 				</div>
+
+        <div className="sign-up">
+          <h2>Don't have an account?</h2>
+          <button onClick={this.signup}> Sign up.</button>
+        </div>
 			</div>
+    </div>
 		);
 	}
 
